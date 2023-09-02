1d5cb094dfed528b3bf4631f4d1e891e11d74ac3
Your Name~ <you@example.com>
2023-09-02 22:26:13 +0600

    animation added

--- app.js
+++ app.js
@@ -5,38 +5,47 @@ let navbar = document.querySelector('.navbar');
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('fa-xmark');
     navbar.classList.toggle('active');
-}   
+}
 //scroll section
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
 
-window.onscroll =()=>{
+window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 100;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if(top >= offset && top < offset + height){
+        if (top >= offset && top < offset + height) {
             //active navbar links
-            navLinks.forEach(links =>{
+            navLinks.forEach(links => {
                 links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id +']').classList.add('active')
-
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             })
+            // active section for animation on scroll
+            sec.classList.add('show-animate');
+        }
+        // if warnt to use animation repeats on scroll use this
+        else {
+            sec.classList.remove('show-animate');
+
         }
     })
     //sticky header
     let header = document.querySelector('header');
 
-    header.classList.toggle('sticky',window.scrollY > 100)
+    header.classList.toggle('sticky', window.scrollY > 100)
 
     // remove toggle icon and navbar when click navbar links (scroll)
 
     menuIcon.classList.remove('fa-xmark');
-    navbar.classList.remove ('active');
+    navbar.classList.remove('active');
 
+    // animate footer on scroll
+    let footer = document.querySelector('footer');
+    footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
 
 
 }
